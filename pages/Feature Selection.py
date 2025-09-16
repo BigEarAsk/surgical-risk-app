324bb7932aa6b73b24a93feae352eb34d9864e4d
BigEarAsk <91580869+BigEarAsk@users.noreply.github.com>
2025-09-16 21:37:22 +0800

    Create Feature Selection.py

---
+++ pages/Feature Selection.py
@@ -0,0 +1,58 @@
+import pandas as pd
+import streamlit as st
+from utils.select_var import select_feature
+
+st.title("1️⃣ Feature Selection")
+
+df = st.session_state.train_df
+
+var_names = df.columns.tolist()
+
+target_var = st.selectbox("Select target variable", df.columns)
+X = df.drop(columns=[target_var])
+y = df[target_var]
+
+available_methods = [
+        'Genetic Algorithm',
+        'Lasso',
+        'Random Forest-based Recursive Feature Elimination (RF-RFE)'
+    ]
+
+selected_methods = st.multiselect(
+        "Select method(s) to select features",
+        options = available_methods,
+        default = ["Genetic Algorithm"]  # 可自定义默认值
+    )
+
+results = select_feature(selected_methods,X,y,var_names)
+# print(results)
+if len(selected_methods) > 1:
+    selected_features = []
+    for res in results:
+        selected_features.extend(res)
+    selected_features = list(set(selected_features))
+    st.write(f'After {len(selected_methods)} methods，the selected features are：',
+             selected_features)
+    results = selected_features
+    
+else:
+    # results = results[0]
+    st.write('The final selected features are：',results)
+
+X = df.drop(columns=[target_var])
+X = X.loc[:,results]
+y = df[target_var]
+
+st.session_state.X_train = X
+st.session_state.y_train = y
+st.session_state.target_var = target_var
+
+df2 = st.session_state.validation_df 
+X_val = df2.drop(columns=[target_var])
+X_val = X_val.loc[:,results]
+y_val = df2[target_var]
+st.session_state.X_val = X_val
+st.session_state.y_val = y_val
+
+st.session_state.X_combined = pd.concat([st.session_state.X_train,st.session_state.X_val],ignore_index=True)
+st.session_state.df_combined = pd.concat([st.session_state.train_df,st.session_state.validation_df],ignore_index=True)
