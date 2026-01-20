3f57124c136c888541bc167170103374ba3a3293
BigEarAsk <91580869+BigEarAsk@users.noreply.github.com>
2026-01-20 22:39:55 +0800

    Update Precision-Recall Curve (PRC).py

--- pages/Precision-Recall Curve (PRC).py
+++ pages/Precision-Recall Curve (PRC).py
@@ -1,29 +1,36 @@
 import io
 import streamlit as st
 from utils.pr_plot import draw_pr
+from common import render_lang_toggle, t
 
-st.title("1️⃣ PR plot")
+render_lang_toggle(location="sidebar")  # ✅ 这一行保证按钮
+# st.title("1️⃣ PR plot")
+st.title(t("pr_title"))
 
 if "models_train" not in st.session_state:
-    st.warning("⬅️ Please train model on the Training page first.")
+    # st.warning("⬅️ Please train model on the Training page first.")
+    st.warning(t("pdp_warning"))
 else:
 
     selected_models_test = st.selectbox(
-        "Select one model to test or display",
+        # "Select one model to test or display",
+        t('pdp_choose_model'),
         options=st.session_state.selected_models,
         # default=None  # 可自定义默认值
     )
 
     st.session_state.model_test = selected_models_test
 
-    st.info("The PR figure is drawing...")
+    # st.info("The PR figure is drawing...")
+    st.info(t("pr_waiting"))
 
     fig = draw_pr(st.session_state.model_test,st.session_state.models_train,
                    st.session_state.X_train,st.session_state.y_train,
                    st.session_state.X_val,st.session_state.y_val,
                    st.session_state.target_var)
 
-    st.info("The PR figure has finished!")
+    # st.info("The PR figure has finished!")
+    st.info(t('pr_finish'))
 
     st.pyplot(fig)
 
@@ -33,7 +40,8 @@ else:
 
     # ✅ 下载按钮
     st.download_button(
-        label="📥 Download PR plot as PNG",
+        # label="📥 Download PR plot as PNG",
+        label=t("pr_download"),
         data=buf,
         file_name="PR_plot.png",
         mime="image/png"
