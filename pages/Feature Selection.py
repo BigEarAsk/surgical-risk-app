e0614886cde815b5c52b370ce3af3c0ecece3f0f
BigEarAsk <91580869+BigEarAsk@users.noreply.github.com>
2026-01-20 22:54:32 +0800

    Update Feature Selection.py

--- pages/Feature Selection.py
+++ pages/Feature Selection.py
@@ -14,57 +14,57 @@ if "train_df" not in st.session_state:
 else:
     df = st.session_state.train_df
 
-var_names = df.columns.tolist()
-
-# target_var = st.selectbox("Select target variable", df.columns)
-target_var = st.selectbox(t("feature_select"), df.columns)
-X = df.drop(columns=[target_var])
-y = df[target_var]
-
-available_methods = [
-        'Genetic Algorithm',
-        'Lasso',
-        'Random Forest-based Recursive Feature Elimination (RF-RFE)'
-    ]
-
-selected_methods = st.multiselect(
-        # "Select method(s) to select features",
-        t("feature_model"),
-        options = available_methods,
-        default = ["Genetic Algorithm"]  # 可自定义默认值
-    )
-
-results = select_feature(selected_methods,X,y,var_names)
-# print(results)
-if len(selected_methods) > 1:
-    selected_features = []
-    for res in results:
-        selected_features.extend(res)
-    selected_features = list(set(selected_features))
-    # st.write(f'After {len(selected_methods)} methods，the selected features are：',
-    #          selected_features)
-    st.write(t('feature_finish'), selected_features)
-    results = selected_features
+    var_names = df.columns.tolist()
     
-else:
-    # results = results[0]
-    # st.write('The final selected features are：',results)
-    st.write(t('feature_finish'), results)
-
-X = df.drop(columns=[target_var])
-X = X.loc[:,results]
-y = df[target_var]
-
-st.session_state.X_train = X
-st.session_state.y_train = y
-st.session_state.target_var = target_var
-
-df2 = st.session_state.validation_df 
-X_val = df2.drop(columns=[target_var])
-X_val = X_val.loc[:,results]
-y_val = df2[target_var]
-st.session_state.X_val = X_val
-st.session_state.y_val = y_val
-
-st.session_state.X_combined = pd.concat([st.session_state.X_train,st.session_state.X_val],ignore_index=True)
-st.session_state.df_combined = pd.concat([st.session_state.train_df,st.session_state.validation_df],ignore_index=True)
+    # target_var = st.selectbox("Select target variable", df.columns)
+    target_var = st.selectbox(t("feature_select"), df.columns)
+    X = df.drop(columns=[target_var])
+    y = df[target_var]
+    
+    available_methods = [
+            'Genetic Algorithm',
+            'Lasso',
+            'Random Forest-based Recursive Feature Elimination (RF-RFE)'
+        ]
+    
+    selected_methods = st.multiselect(
+            # "Select method(s) to select features",
+            t("feature_model"),
+            options = available_methods,
+            default = ["Genetic Algorithm"]  # 可自定义默认值
+        )
+    
+    results = select_feature(selected_methods,X,y,var_names)
+    # print(results)
+    if len(selected_methods) > 1:
+        selected_features = []
+        for res in results:
+            selected_features.extend(res)
+        selected_features = list(set(selected_features))
+        # st.write(f'After {len(selected_methods)} methods，the selected features are：',
+        #          selected_features)
+        st.write(t('feature_finish'), selected_features)
+        results = selected_features
+        
+    else:
+        # results = results[0]
+        # st.write('The final selected features are：',results)
+        st.write(t('feature_finish'), results)
+    
+    X = df.drop(columns=[target_var])
+    X = X.loc[:,results]
+    y = df[target_var]
+    
+    st.session_state.X_train = X
+    st.session_state.y_train = y
+    st.session_state.target_var = target_var
+    
+    df2 = st.session_state.validation_df 
+    X_val = df2.drop(columns=[target_var])
+    X_val = X_val.loc[:,results]
+    y_val = df2[target_var]
+    st.session_state.X_val = X_val
+    st.session_state.y_val = y_val
+    
+    st.session_state.X_combined = pd.concat([st.session_state.X_train,st.session_state.X_val],ignore_index=True)
+    st.session_state.df_combined = pd.concat([st.session_state.train_df,st.session_state.validation_df],ignore_index=True)
