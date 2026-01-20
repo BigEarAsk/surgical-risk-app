31760811fd0f7482ddb2617dc7801972c32be524
BigEarAsk <91580869+BigEarAsk@users.noreply.github.com>
2026-01-20 22:38:57 +0800

    Update Counterfactual Validation.py

--- pages/Counterfactual Validation.py
+++ pages/Counterfactual Validation.py
@@ -1,14 +1,20 @@
 import io
 import streamlit as st
 from utils.Counterfact import get_res
+from common import render_lang_toggle, t
 
-st.title("1️⃣ Counterfact Verify")
+render_lang_toggle(location="sidebar")  # ✅ 这一行保证按钮
+
+# st.title("1️⃣ Counterfact Verify")
+st.title(t("counterfact_title"))
 
 if "models_train" not in st.session_state:
-    st.warning("⬅️ Please train model on the Training page first.")
+    # st.warning("⬅️ Please train model on the Training page first.")
+    st.warning(t("pdp_warning"))
 else:
     selected_models_test = st.selectbox(
-        "Select one model to test or display",
+        # "Select one model to test or display",
+        t("pdp_choose_model"),
         options=st.session_state.selected_models,
         # default=None  # 可自定义默认值
     )
@@ -16,13 +22,15 @@ else:
     st.session_state.model_test = selected_models_test
 
     selected_features = st.multiselect(
-        "Select features to be changed",
+        # "Select features to be changed",
+        t("counterfact_change"),
         options = st.session_state.X_train.columns,
     )
 
     st.session_state.selected_features_change = selected_features
 
-    st.info("The Counterfact samples are generating...")
+    # st.info("The Counterfact samples are generating...")
+    st.info(t("counterfact_waiting"))
 
     counterfact_df = get_res(selected_models_test,st.session_state.models_train,
                              st.session_state.df_combined,st.session_state.continuous_features,
@@ -30,4 +38,5 @@ else:
 
     st.dataframe(counterfact_df)
 
-    st.info("The Counterfact samples has generated!")
+    # st.info("The Counterfact samples has generated!")
+    st.info(t("counterfact_finish"))
