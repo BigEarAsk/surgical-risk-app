cac77594678ce6533d2d93e16213d2115632ecab
BigEarAsk <91580869+BigEarAsk@users.noreply.github.com>
2026-01-20 22:40:21 +0800

    Update SHAP Value Interpretation.py

--- pages/SHAP Value Interpretation.py
+++ pages/SHAP Value Interpretation.py
@@ -2,21 +2,28 @@ import io
 import streamlit as st
 from utils.Shap_plot import draw
 import pandas as pd
+from common import render_lang_toggle, t
 
-st.title("1️⃣ Shap plot")
+render_lang_toggle(location="sidebar")  # ✅ 这一行保证按钮
+
+# st.title("1️⃣ Shap plot")
+st.title(t("shap_title"))
 
 if "models_train" not in st.session_state:
-    st.warning("⬅️ Please train model on the Training page first.")
+    # st.warning("⬅️ Please train model on the Training page first.")
+    st.warning(t("pdp_warning"))
 else:
     selected_models_test = st.selectbox(
-        "Select one model to test or display",
+        # "Select one model to test or display",
+        t("pdp_choose_model"),
         options=st.session_state.selected_models,
         # default=None  # 可自定义默认值
     )
 
     st.session_state.model_test = selected_models_test
 
-    st.info("The Shap figure is drawing...")
+    # st.info("The Shap figure is drawing...")
+    st.info(t("shap_waiting"))
 
     st.session_state.X_combined = pd.concat([st.session_state.X_train,st.session_state.X_val],ignore_index=True)
     
